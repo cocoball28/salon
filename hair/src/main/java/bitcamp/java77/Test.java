5796d0cf738f411f552b9b89dd41fcfb7bb50186
cocoball28 <jjinoya@hotmail.com>
2016-02-26 18:57:31 +0900

    gh

--- hair/src/main/java/bitcamp/java77/Test.java
+++ hair/src/main/java/bitcamp/java77/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("dd");
 		System.out.println("용근 수정함");
+
+		String aa = "jinho";
+
 		System.out.println("차노 수정함");
+
 	}
 }
