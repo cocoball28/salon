69800514ecb49a750917d69ae6803e1d4c0e2b7c
pulby1989 <pulby1989@naver.com>
2016-03-10 17:29:42 +0900

    z

--- hair/src/main/java/salon/member/controller/MemberController.java
+++ hair/src/main/java/salon/member/controller/MemberController.java
@@ -3,6 +3,8 @@ package salon.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import salon.member.dao.MemberDao;
 import salon.member.domain.Member;
@@ -14,8 +16,10 @@ public class MemberController {
 	@Autowired MemberDao memberDao;
 	@Autowired MemberService memberService;
 	
-	@RequestMapping("regist")
+	
+	@RequestMapping(value="regist", method=RequestMethod.POST)
+	@ResponseBody
 	public void regist(Member member) {
-		
+		System.out.println("들어왔다");
 	}
 }
--- hair/src/main/java/salon/member/service/impl/MemberServiceImpl.java
+++ hair/src/main/java/salon/member/service/impl/MemberServiceImpl.java
@@ -1,11 +1,13 @@
 package salon.member.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import salon.member.dao.MemberDao;
 import salon.member.domain.Member;
 import salon.member.service.MemberService;
 
+@Service
 public class MemberServiceImpl implements MemberService {
 	
 	@Autowired MemberDao memberDao;
