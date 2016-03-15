7d134ce7939b98b4e7004b36106d38ae08d671d8
pulby1989 <pulby1989@naver.com>
2016-03-15 12:51:55 +0900

    z

--- hair/src/main/webapp/salonpage/salon/js/commonjs.js
+++ hair/src/main/webapp/salonpage/salon/js/commonjs.js
@@ -1,1 +1,10 @@
-var shopPath = 'http://192.168.0.5:8008/salon';
+//var shopPath = 'http://192.168.0.5:8008/salon';
+
+function getContextPath(){
+    var offset=location.href.indexOf(location.host)+location.host.length;
+    var ctxPath=location.href.substring(offset,location.href.indexOf('/',offset+1));
+    return ctxPath;
+}
+
+
+var contextPath =  getContextPath();
