85f4ee9325bb342d5170590b4b4e51a3513a9ff6
cocoball28 <jjinoya@hotmail.com>
2016-03-22 17:13:09 +0900

    fgh

--- hair/src/main/java/salon/domain/Shop.java
+++ hair/src/main/java/salon/domain/Shop.java
@@ -2,14 +2,15 @@ package salon.domain;
 
 public class Shop {
 	private int no;
+	private String name;
+	private String phoName;
+	
 	public int getNo() {
 		return no;
 	}
 	public void setNo(int no) {
 		this.no = no;
 	}
-	private String name;
-	private String phoName;
 	public String getName() {
 		return name;
 	}
