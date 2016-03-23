612cc724e512943df8867a8630cb736b3c19ac90
pulby1989 <pulby1989@naver.com>
2016-03-23 18:39:51 +0900

    gn

--- hair/디비스크립트/salonpage/shopImage.sql
+++ hair/디비스크립트/salonpage/shopImage.sql
@@ -2,7 +2,18 @@
 create table shopImage (
 	file_no int auto_increment primary key,
 	file_name varchar(200),
-	no int
+	shop_no int not null
 );
 
+
+select member.name, dsn.no
+  		  from member inner join dsn
+     	    on member.no = dsn.no
+ 		 order by dsn.no;
+
+
+drop table shopImage;
 select * from shopImage;
+
+select file_name as fileName, file_no as fileNo, shop_no as no 
+  		  from shopImage
