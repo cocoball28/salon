6553788be3309dd729c9e9420ee495a3ec3e309a
pulby1989 <pulby1989@naver.com>
2016-03-23 18:33:29 +0900

    df

--- hair/src/main/java/salon/service/ShopService.java
+++ hair/src/main/java/salon/service/ShopService.java
@@ -1,12 +1,13 @@
 package salon.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import salon.domain.Shop;
 
 public interface ShopService {
-	List<Shop> selectList();
+	public Map<String, Object> selectList();
 	public List<Shop> register(Shop shop, MultipartHttpServletRequest mRequest) throws Exception;
 }
