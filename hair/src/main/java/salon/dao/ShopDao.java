4ee8113e5070c4ec971122c16bc6a2d1be7a78c3
pulby1989 <pulby1989@naver.com>
2016-03-23 18:30:33 +0900

    d

--- hair/src/main/java/salon/dao/ShopDao.java
+++ hair/src/main/java/salon/dao/ShopDao.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import salon.domain.Designer;
 import salon.domain.Shop;
 import salon.domain.ShopImage;
 
 @Repository
 public interface ShopDao {
 	public List<Shop> selectShopList();
+	public List<Designer> selectDesignerList();
+	public List<ShopImage> selectShopImageList();
 	public void insertImage(ShopImage shopImage);
 	public List<Shop> getShop(Shop shop);
 }
