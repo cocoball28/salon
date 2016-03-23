26398be8371199e41a85176b3eb5a7d36cbf8254
pulby1989 <pulby1989@naver.com>
2016-03-23 18:44:55 +0900

    z

---
+++ hair/src/main/webapp/map/js/commonjs.js
@@ -0,0 +1,10 @@
+//var shopPath = 'http://192.168.0.7:8008/salon';
+
+function getContextPath(){
+    var offset=location.href.indexOf(location.host)+location.host.length;
+    var ctxPath=location.href.substring(offset,location.href.indexOf('/',offset+1));
+    return ctxPath;
+}
+
+
+var contextPath =  getContextPath();
