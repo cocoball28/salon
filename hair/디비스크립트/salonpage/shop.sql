463671bbc6bfd1afe30192d4196c09dc206d5a81
cocoball28 <jjinoya@hotmail.com>
2016-03-24 15:56:50 +0900

    gjikul

--- hair/디비스크립트/salonpage/shop.sql
+++ hair/디비스크립트/salonpage/shop.sql
@@ -7,11 +7,11 @@ create table shop (
 	addr varchar(150) not null
 );
 
-insert into shop (sano, name, tel, zip, addr)
+insert into shop (name, tel, zip, addr)
 	 	  values ("juno hair", 027123721, 182318, "서울특별시 서초구 강남대로 369");
-insert into shop (sano, name, tel, zip, addr)
+insert into shop (name, tel, zip, addr)
 	 	  values ("choice hair", 0317125723, 231318, "서울특별시 강남구 강남대로 372");
-insert into shop (sano, name, tel, zip, addr)
+insert into shop (name, tel, zip, addr)
 	 	  values ("hq", 027152314, 125848, "서울특별시 서초구 서초대로74길 11");
 drop table shop;
 select sano, name from shop;
