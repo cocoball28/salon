1589ea7de89eb56de48fe88cfc745b91d87f6454
cocoball28 <jjinoya@hotmail.com>
2016-03-24 15:55:38 +0900

    fg

--- hair/디비스크립트/salonpage/shop.sql
+++ hair/디비스크립트/salonpage/shop.sql
@@ -1,17 +1,17 @@
 테이블 생성
 create table shop (
-	sano int primary key,
+	sano int primary key auto_increment,
 	name varchar(20) not null,
 	tel int not null,
 	zip int not null,
 	addr varchar(150) not null
-)
+);
 
 insert into shop (sano, name, tel, zip, addr)
-	 	  values (1, "juno", 027123721, 182318, "서울특별시 서초구 강남대로 369");
+	 	  values ("juno hair", 027123721, 182318, "서울특별시 서초구 강남대로 369");
 insert into shop (sano, name, tel, zip, addr)
-	 	  values (2, "choice", 0317125723, 231318, "서울특별시 강남구 강남대로 372");
+	 	  values ("choice hair", 0317125723, 231318, "서울특별시 강남구 강남대로 372");
 insert into shop (sano, name, tel, zip, addr)
-	 	  values (3, "hq", 027152314, 125848, "서울특별시 서초구 서초대로74길 11");
+	 	  values ("hq", 027152314, 125848, "서울특별시 서초구 서초대로74길 11");
 drop table shop;
 select sano, name from shop;
