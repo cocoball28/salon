4f59d9ae9bb7213a5bd9b0285c74a9297b298bf9
cocoball28 <jjinoya@hotmail.com>
2016-04-01 16:49:30 +0900

    dddddd

---
+++ hair/src/main/java/salon/domain/Favorite.java
@@ -0,0 +1,49 @@
+package salon.domain;
+
+public class Favorite {
+	private int mno;
+	private int bno;
+	private int sano;
+	private int mdno;
+	private int fav;
+	private String from;
+	
+	public int getMno() {
+		return mno;
+	}
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public int getSano() {
+		return sano;
+	}
+	public void setSano(int sano) {
+		this.sano = sano;
+	}
+	public int getMdno() {
+		return mdno;
+	}
+	public void setMdno(int mdno) {
+		this.mdno = mdno;
+	}
+	public int getFav() {
+		return fav;
+	}
+	public void setFav(int fav) {
+		this.fav = fav;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	
+	
+}
